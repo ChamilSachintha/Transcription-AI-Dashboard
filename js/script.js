ed51391c91546b0875742efeca8c60a2ffda4fe2
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2025-03-23 00:38:03 +0530

    Add comment to dropdown icon changer js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-  function toggleDropdown() {
-    const menu = document.getElementById("dropdownMenu");
-    const icon = document.getElementById("dropdownIcon");
+// Change dropdown icon
+function toggleDropdown() {
+  const menu = document.getElementById("dropdownMenu");
+  const icon = document.getElementById("dropdownIcon");
 
-    menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
 
-    if (!menu.classList.contains("hidden")) {
-      icon.src = "./images/icon-up.png"; // Change to up icon
-    } else {
-      icon.src = "./images/icon-down.png"; // Change back to down icon
-    }
+  if (!menu.classList.contains("hidden")) {
+    icon.src = "./images/icon-up.png"; // Change to up icon
+  } else {
+    icon.src = "./images/icon-down.png"; // Change back to down icon
   }
+}
