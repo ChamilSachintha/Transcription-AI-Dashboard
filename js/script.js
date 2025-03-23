60ed60919879da4e428fce0ef875d6bc8dc90c51
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2025-03-23 19:32:45 +0530

    Remove toggle icon due to deployment error

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-// Change dropdown icon
+// Dropdown menu
 function toggleDropdown() {
   const menu = document.getElementById("dropdownMenu");
-  const icon = document.getElementById("dropdownIcon");
-
   menu.classList.toggle("hidden");
-
-  if (!menu.classList.contains("hidden")) {
-    icon.src = "./images/icon-up.png"; // Change to up icon
-  } else {
-    icon.src = "./images/icon-down.png"; // Change back to down icon
-  }
 }
