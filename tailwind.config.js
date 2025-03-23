6fc0cd47ad7695241966409408f5bea38eae5f29
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2025-03-23 20:25:47 +0530

    Add Neue Haas Grotesk Display Pro font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"], 
+  content: ["./index.html", "./src/**/*.{html,js,woff2,woff}"], 
   theme: {
     extend: {
       fontFamily: {
         aeonik: ['Aeonik', 'sans-serif'],
+        haas: ['Neue Haas Grotesk Display Pro', 'sans-serif'],
       },
     },
   },
